e22cb4479a3ecdc5692d21b74bac878b3d552e50
Marina Cantarelli <macbarroca@gmail.com>
2024-07-16 13:25:18 -0300

    Challenge 5

---
+++ Challenge5.py
@@ -0,0 +1,17 @@
+num1 = float(input('Digite o primeiro numero: '))
+num2 = float(input('Digite o segundo numero: '))
+
+add = num1 + num2
+print(f'Soma: {add}')
+
+sub = num1 - num2
+print(f'Subtração: {sub}')
+
+multi = num1 * num2
+print(f'Multiplicação: {multi}')
+
+div = num1 / num2 
+print(f'Divisão: {div}')
+
+expo = num1 ** num2
+print(f'Exponenciação: {expo}') 
