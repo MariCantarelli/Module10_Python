73ff19f6be4dbbd545b065dd97cd83b690b440f1
Marina Cantarelli <macbarroca@gmail.com>
2024-07-16 15:07:36 -0300

    Challenge 14

---
+++ Challenge14.py
@@ -0,0 +1,11 @@
+for num in range(1, 11):
+    if num > 5:
+        break
+    print(num)
+
+print('========================')
+
+for num in range(1, 11):
+    if num == 5:
+        continue
+    print(num)
