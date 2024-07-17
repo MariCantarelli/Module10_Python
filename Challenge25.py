84691dfb1a2dd240d083b2dd9bb1867ca99312db
Marina Cantarelli <macbarroca@gmail.com>
2024-07-17 00:16:59 -0300

    Challenge 25

---
+++ Challenge25.py
@@ -0,0 +1,8 @@
+number1 = int(input('Digite o primeiro numero: '))
+number2 = int(input('Digite o segundo numero: '))
+
+def soma(num1, num2):
+    result = num1 + num2
+    return result
+
+print(f'Este é o resultado da soma: {soma(number1, number2)}')
