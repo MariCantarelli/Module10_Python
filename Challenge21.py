87c9ee11c00c315bf2609a0e4039ce203857f4b0
Marina Cantarelli <macbarroca@gmail.com>
2024-07-16 23:32:55 -0300

    Challenge 21

---
+++ Challenge21.py
@@ -0,0 +1,8 @@
+input = input('Digite a cidade: ')
+
+list = ('São Paulo', 'Rio de Janeiro', 'Salvador')
+
+if input in list:
+    print('A cidade está na lista de cidades.')
+else: 
+    print('A cidade não está na lista de cidades.')
